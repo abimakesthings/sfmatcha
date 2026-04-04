1845faf4cee481b2faed05612df21759bcf92c03
abi <abitriesthings@gmail.com>
2026-04-03 21:43:50 -0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import TopTen from './components/TopTen'
 import PopularityChart from './components/PopularityChart'
 import StandoutFlavors from './components/StandoutFlavors'
 import PollSection from './components/PollSection'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <PopularityChart />
       <StandoutFlavors />
       <PollSection />
+      <Footer />
     </>
   )
 }
